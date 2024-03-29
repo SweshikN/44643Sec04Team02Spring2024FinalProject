d5e66149016205670ac3f82d8ff958e70e7ca123
Kaushik Ram Koteshwar Venna <vennakaushikramkoteshwar@gmail.com>
2024-03-29 02:23:19 -0500

    Kaushik Ram Koteshwar Venna: Created coco couch classes for login page and signup page.

---
+++ LoginPageVC.swift
@@ -0,0 +1,29 @@
+//
+//  LoginPageVC.swift
+//  Car Pooling
+//
+//  Created by Kaushik Ram Koteshwar Venna on 3/29/24.
+//
+
+import UIKit
+
+class LoginPageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
