29ee4ac3fb3f846da5128b19dfe379e9e170869a
Sneha Kancharla <142957818+Sneha024@users.noreply.github.com>
2024-03-29 21:47:34 -0500

    created AccountVC using coco-touch  and performed segue from AccountVC to SignupVC.

---
+++ Car Pooling/Car Pooling/AccountVC.swift
@@ -0,0 +1,29 @@
+//
+//  AccountVC.swift
+//  Car Pooling
+//
+//  Created by Sneha Kancharla on 3/29/24.
+//
+
+import UIKit
+
+class AccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
