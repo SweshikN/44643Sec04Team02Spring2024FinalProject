6ed4fe4a403bb321617d581e935cb4fbf22ea7aa
Kaushik Ram Koteshwar Venna <vennakaushikramkoteshwar@gmail.com>
2024-04-11 06:08:34 -0500

    Kaushik Ram Koteshwar Venna: Added 2 new cocotouch classes and connected them to view controllers.

---
+++ GiveRideVC.swift
@@ -0,0 +1,29 @@
+//
+//  GiveRideVC.swift
+//  Car Pooling
+//
+//  Created by Kaushik Ram Koteshwar Venna on 4/11/24.
+//
+
+import UIKit
+
+class GiveRideVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
