f6dc2e105937f05994ed4153ed6a9a4e547314a7
Sneha Kancharla <142957818+Sneha024@users.noreply.github.com>
2024-04-12 20:45:51 -0500

    created SplashVC page and cocotouch class to it and given outlet connections

---
+++ Car Pooling/Car Pooling/SplashVC.swift
@@ -0,0 +1,33 @@
+//
+//  SplashVC.swift
+//  Car Pooling
+//
+//  Created by Sneha Kancharla on 4/12/24.
+//
+
+import UIKit
+
+class SplashVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func letsstartBTN(_ sender: UIButton) {
+    }
+    @IBOutlet var imgIV: UIImageView!
+    
+    @IBOutlet var startBTN: UIButton!
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
